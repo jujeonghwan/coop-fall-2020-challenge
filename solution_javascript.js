cdd81a0f56e3a2d4272c49fe113295d567273f1e
jujeonghwan <jujeonghwan@gmail.com>
2020-07-10 14:38:20 -0400

    coop-fall-2020-challenge Jeonghwan Ju

--- solution_javascript.js
+++ solution_javascript.js
@@ -1,14 +1,78 @@
 class EventSourcer {
+  
   constructor() {
-    this.value = 0;
+    this.num_arr = [];  // num list
+    this.cur_pos = 0;   // current position
+    this.value = 0;     // value
   }
 
-  add(num) {}
-  subtract(num) {}
-  undo() {}
-  redo() {}
-  bulk_undo(num) {}
-  bulk_redo(num) {}
+  add(num) {
+    this.value += num;   
+    
+    if (this.cur_pos === this.num_arr.length) {
+      this.num_arr.push(num); // Add to num list
+    }
+    else {
+      this.num_arr[this.cur_pos] = num;
+    }
+    
+    this.cur_pos += 1;
+  }
+
+  subtract(num) {
+    this.value -= num;    
+    
+    if (this.cur_pos === this.num_arr.length) {
+      this.num_arr.push(-num); // Add to num list (minus)
+    }
+    else {
+      this.num_arr[this.cur_pos] = -num;
+    }
+
+    this.cur_pos += 1;
+  }
+
+  undo() {
+    // Check position
+    if (this.cur_pos <= 0) {
+      return;
+    }
+
+    // Undo
+    this.value -= this.num_arr[this.cur_pos - 1];
+
+    // Move cur position (-1)
+    this.cur_pos -= 1;
+  }
+
+  redo() {
+    // Check position
+    if (this.cur_pos > this.num_arr.length - 1) {
+      return;
+    }
+
+    // Redo
+    this.value += this.num_arr[this.cur_pos];
+
+    // Move cur position (+1)
+    this.cur_pos += 1;
+  }
+
+  bulk_undo(num) {
+    // Loop
+    for (var i = 0; i < num; i++) {
+      // execute undo()
+      this.undo();
+    }
+  }
+
+  bulk_redo(num) {
+    // Loop
+    for (var i = 0; i < num; i++) {
+      // execute redo()
+      this.redo();
+    }
+  }
 }
 
 // ----- Do not modify anything below this line (needed for test suite) ------
